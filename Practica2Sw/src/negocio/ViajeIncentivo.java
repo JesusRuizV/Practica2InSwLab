7811fa4c76f495beb6de7fb5ef5a43c165bfb620
JesusRuizV <jesusruizv12@gmail.com>
2019-04-26 14:20:59 -0500

    Initial commit

---
+++ Practica2Sw/src/negocio/ViajeIncentivo.java
@@ -0,0 +1,37 @@
+package negocio;
+
+import java.util.Date;
+
+public class ViajeIncentivo extends Viaje {
+	
+	private String empresa;
+	
+	
+	 //Constructor, getters and setters
+	 public ViajeIncentivo(String origen, String destino, int costo, Date fechaSalida, Date fechaLlegada, String pEmpresa) {
+			super(origen, destino, costo, fechaSalida, fechaLlegada);
+			this.empresa=pEmpresa;
+		}
+	 
+	 
+	 public String getEmpresa() {
+		return empresa;
+	}
+
+
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+
+
+	@Override
+	 public String descripcion() {
+	 return "Viaje incentivo que te envia la empresa " + empresa;
+	 }
+
+	 public String cualquierMetodo2() {
+	 return "Método implementado en la clase hija viaje de incentivo";
+	 }
+
+
+}
